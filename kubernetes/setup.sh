4f78d66e8dc663162bd8d63f69caa2a799c8b7ee
Louis Dutoit <louis.dutoitld@gmail.com>
2024-11-23 20:07:42 +0100

    wip: k8s init

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -6,4 +6,5 @@ kubectl create namespace argocd
 kubectl apply -n argocd-f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl port-forward service/argocd-server -n argocd 8080:443
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"name": "http", "nodePort": 30080, "port": 80, "protocol": "TCP", "targetPort": 8080}, {"name": "https", "nodePort": 30443, "port": 443, "protocol": "TCP", "targetPort": 8080}]}}'
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
