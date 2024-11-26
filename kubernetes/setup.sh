a8f221b3b010fba913f6019e9dc92b4acd6a3489
Louis Dutoit <louis.dutoitld@gmail.com>
2024-11-26 14:55:39 +0100

    wip: k8s

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Setup cluster
 kind create cluster --config kind-config.yaml
 
+# Install Ingress Controller for Kind
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+
+# Install ArgoCD
 kubectl create namespace argocd
-kubectl apply -n argocd-f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl port-forward service/argocd-server -n argocd 8080:443
 #kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"name": "http", "nodePort": 30080, "port": 80, "protocol": "TCP", "targetPort": 8080}, {"name": "https", "nodePort": 30443, "port": 443, "protocol": "TCP", "targetPort": 8080}]}}'
